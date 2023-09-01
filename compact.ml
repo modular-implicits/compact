da75e792ce40081ed4f39bea9c2fa40b6f2f5750
Patrick Reader <_@pxeger.com>
2023-09-01 14:44:57 +0100

    wip

--- compact.ml
+++ compact.ml
@@ -1,99 +1,132 @@
-module Generic = Generics_core
+open Imp.Any
 
-let ((lor), (land), (asr), (lsr), (lsl), (lxor), lnot, zero, one) = Int64.
-  (logor, logand, shift_right, shift_right_logical, shift_left, logxor, lognot, of_int 0, of_int 1)
-
-module type Compact = sig
+module type Product = sig
+  type a
+  type b
   type t
-
-  (* must return a number between 0 and 2^bit_width *)
-  val compress : t -> int64
-
-  (* must only depend on the 2^bit_width least significant bits *)
-  val decompress : int64 -> t
-
-  (* TODO: is int64 overflow defined behaviour in ocaml? *)
-  (* must be <= 64 *)
-  val bit_width : int
+  val construct : a code -> b code -> t code
+  val deconstruct : t code -> a code * b code
 end
 
-implicit module Compact_Char: Compact with type t = char = struct
-  type t = char
-  let compress x = Int64.of_int (Char.code x)
-  let decompress x = Char.chr (Int64.to_int x)
-  let bit_width = 8
-end
-
-implicit module Compact_Unit : Compact with type t = unit
+implicit module Pair {A: Any} {B: Any}
+:  Product
+  with type t = A.t * B.t
+  and type a = A.t
+  and type b = B.t
 = struct
-  type t = unit
-  let compress () = zero
-  let decompress _ = ()
-  let bit_width = 0
+  type t = A.t * B.t
+  type a = A.t
+  type b = B.t
+  let construct a b = .< .~a, .~b >.
+  let deconstruct p = .< fst .~p >., .< snd .~p >.
 end
 
-implicit module Compact_Sum {X: Compact} {Y: Compact} :
-  Compact with type t = (X.t, Y.t) Generic.sum
+module type Sum = sig
+  type a
+  type b
+  type t
+  val construct_a : a code -> t code
+  val construct_b : b code -> t code
+  val match_ :
+    (a code -> 'r code) ->
+    (b code -> 'r code) ->
+    t code ->
+    'r code
+end
+
+implicit module Option {A: Any}
+: Sum
+  with type t = A.t option
+  and type a = A.t
+  and type b = unit
 = struct
-  type t = (X.t, Y.t) Generic.sum
+  type t = A.t option
+  type a = A.t
+  type b = unit
+  let construct_a a = .< Some .~a >.
+  let construct_b _ = .< None >.
+  let match_ fa fb x = .<
+    match .~x with
+    | Some a -> .~(fa .< a >.)
+    | None -> .~(fb .< () >.)
+  >.
+end
 
-  (* note: if x and y don't have the same bit width, we just implicitly left-pad with 0s
-     e.g. if x is 5 bits wide and y is 7 bits wide, then:
-
-     Left x  -> 00xxxxx0
-     Right y -> yyyyyyy1
+module type Sized = sig
+  (* Compute some notion of "size", in arbitrary units.
+     This will bare little relation to the actual amount of memory the object takes up.
    *)
-
-  let compress = function
-    | Generic.Left x -> X.compress x lsl 1 lor zero
-    | Generic.Right y -> Y.compress y lsl 1 lor one
-
-  let decompress c =
-    let flag = c land one in
-    let c' = c lsr 1 in
-    if flag = zero
-    then Generic.Left (X.decompress c')
-    else Generic.Right (Y.decompress c')
-
-  let bit_width = 1 + max X.bit_width Y.bit_width
+  type t
+  val size : t -> int
 end
 
-implicit module Compact_Prod {X: Compact} {Y: Compact} :
-  Compact with type t = (X.t, Y.t) Generic.prod
+module type Sized_ = sig
+  type t
+  val size : t code -> int code
+end
+
+implicit module Sized_Product
+  {P: Product}
+  {A: Sized_ with type t = P.a}
+  {B: Sized_ with type t = P.b}
+  : Sized_ with type t = P.t
 = struct
-  type t = (X.t, Y.t) Generic.prod
-
-  let compress (Generic.Prod (x, y)) =
-    X.compress x lsl Y.bit_width lor Y.compress y
-
-  let decompress c =
-    Generic.Prod (X.decompress (c lsr Y.bit_width), Y.decompress c)
-
-  let bit_width = X.bit_width + Y.bit_width
+  type t = P.t
+  let size x =
+    let (a, b) = P.deconstruct x in
+    .< .~(A.size a) + .~(B.size b) >.
 end
 
-implicit module Compact_Basic {X: Compact} :
-  Compact with type t = X.t Generic.basic
+implicit module Sized_Sum
+  {S: Sum}
+  {A: Sized_ with type t = S.a}
+  {B: Sized_ with type t = S.b}
+  : Sized_ with type t = S.t
 = struct
-  type t = X.t Generic.basic
-  let compress (Generic.Basic (_, x)) = X.compress x
-  (* TODO: what to do about the label here? *)
-  let decompress c = Generic.Basic ("%decompressed", (X.decompress c))
-  let bit_width = X.bit_width
+  type t = S.t
+  let size x = 
+    .< 1 + .~(S.match_ A.size B.size x) >.
 end
 
-(* not marked implicit to avoid overlapping instances
-   You can opt-in if you want to use this, by writing:
-    ```
-    let implicit module Compact_Generic = Compact_Generic in
-    (* ... *)
-    ```
+(* The __basic__ marker acts as a "stop" for the implicit module resolver.
+   Without it, the implicit functors Sized_ and Sized_Basic would form an infinite loop
  *)
-module Compact_Generic {X: Generic.Generic} {XRep: Compact with type t = X.rep}
-  : Compact with type t = X.t
-= struct
-  type t = X.t
-  let compress x = XRep.compress (X.toRep x)
-  let decompress c = X.fromRep (XRep.decompress c)
-  let bit_width = XRep.bit_width
+module type Sized_Basic = sig
+  include Sized
+  val __basic__ : unit
 end
+
+implicit module Sized_Basic
+  {T: Sized_Basic}
+  : Sized_ with type t = T.t = struct
+  type t = T.t
+  let size x = .< T.size .~x >.
+end
+
+implicit module Sized_Unit : Sized_Basic with type t = unit = struct
+  type t = unit
+  let __basic__ = ()
+  let size () = 1
+end
+
+implicit module Sized_Int : Sized_Basic with type t = int = struct
+  type t = int
+  let __basic__ = ()
+  let size _ = 4
+end
+
+implicit module Sized_List {S: Sized} : Sized_Basic with type t = S.t list = struct
+  type t = S.t list
+  let __basic__ = ()
+  (* until we have some way of dealing with inductive types, this will have to do *)
+  let rec size = function
+    | [] -> 1
+    | x :: xs -> 1 + S.size x + size xs
+end
+
+implicit module Sized_ {S: Sized_} : Sized with type t = S.t = struct
+  type t = S.t
+  let size x = Runcode.run (S.size .< x >.)
+end
+
+let size {S: Sized} x = S.size x
--- tests/test.ml
+++ tests/test.ml
@@ -1,1 +1,8 @@
-let () = assert true
+open Imp.Any
+open Compact
+
+let thing : (int * int) option = Some (3, 6)
+
+let () = Printf.printf "%d" (size thing)
+
+let () = ignore (module Any_Int : Any)
